c730f198f09f184e7a4bf325b00d3d2d6f9c614b
Igor A Tarasov <develop@dicr.org>
2020-02-23 09:51:47 +0500

    update

--- src/FileInputWidgetAsset.php
+++ src/FileInputWidgetAsset.php
@@ -1,18 +1,18 @@
 <?php
 /**
- * @copyright 2019-2019 Dicr http://dicr.org
+ * @copyright 2019-2020 Dicr http://dicr.org
  * @author Igor A Tarasov <develop@dicr.org>
  * @license GPL
- * @version 24.11.19 00:29:11
+ * @version 23.02.20 09:50:18
  */
 
 /** @noinspection ClassOverridesFieldOfSuperClassInspection */
 
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace dicr\file;
 
-use yii\jui\JuiAsset;
+use dicr\asset\JuiAsset;
 use yii\web\AssetBundle;
 use yii\web\JqueryAsset;
 
